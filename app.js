c424d23632c0dbf6e1ed987714e086671f3e656e
ShankarAdd <shankarraibagi777@gmail.com>
2023-10-10 19:21:43 +0530

    addingProduct

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express=require('express');
 
+const bodyParser=require('body-parser');
+
 const app=express();
 
-app.use((req,res,next) =>{
-    console.log('In middleware');
-    next();
-}); 
-app.use((req,res,next) =>{
-    console.log('In another middleware');
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/add-product',(req,res,next) =>{
+    res.send('<form action="/product" method="POST"><label for="title">Name of product</label><br><input type="text" name="title"><br><label for="title">Price of product</label><br><input type="text" name="price"><br><br><button type="submit">Add Product</button></form>')
+});
+app.use('/product',(req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next) =>{
     res.send('<h1 style="background-color:red">This is from ExpressJS')
 });
 
